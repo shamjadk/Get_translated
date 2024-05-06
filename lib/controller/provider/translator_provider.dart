c0c25b2cca90b4f04b86ffe88345c32a7d7689eb
unknown <shamjadkarattiyattil@gmail.com>
2024-05-06 10:44:47 +0530

    Create methods for objectbox
    Created add,get,remove and clear history methods for objectbox(Local
    Storage)

--- lib/controller/provider/translator_provider.dart
+++ lib/controller/provider/translator_provider.dart
@@ -2,7 +2,10 @@ import 'dart:developer';
 
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:translate_it/controller/objectbox_store.dart';
 import 'package:translate_it/core/snack_bar.dart';
+import 'package:translate_it/model/history_entity_model.dart';
+import 'package:translate_it/objectbox.g.dart';
 import 'package:translator/translator.dart';
 
 part 'translator_provider.g.dart';
@@ -10,9 +13,11 @@ part 'translator_provider.g.dart';
 @riverpod
 class Translator extends _$Translator {
   GoogleTranslator? translator;
+  late Box<HistoryEntityModel> box;
   @override
   String build() {
     translator = GoogleTranslator();
+    box = HistoryObjectboxStore.instance.historyBox;
     return '';
   }
 
@@ -33,5 +38,21 @@ class Translator extends _$Translator {
       log(e.toString());
       state = '';
     }
+  }
+
+  Future<void> addToHistory(HistoryEntityModel model) async {
+    box.put(model);
+  }
+
+  Future<List<HistoryEntityModel>> getHistory() async {
+    return box.getAll();
+  }
+
+  Future<void> deleteFromHistory(int id) async {
+    box.remove(id);
+  }
+
+  Future<void> clearHistory() async {
+    box.removeAll();
   }
 }
